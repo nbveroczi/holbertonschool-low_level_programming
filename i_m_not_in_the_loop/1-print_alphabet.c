75dd8f2632f2b1fa101ed8b554ba5e4709ea4bc2
nbveroczi <nbveroczi@gmail.com>
2016-02-08 08:08:33 +0000

    alphabet

---
+++ i_m_not_in_the_loop/1-print_alphabet.c
@@ -0,0 +1,12 @@
+#include "my_functions.h"
+/*This is where the comment goes */ 
+
+void print_alphabet(void)
+{
+  char aChar;
+
+  for (aChar = 'a'; aChar <= 'z'; aChar++) 
+  {
+    print_char(aChar);
+  }
+}
