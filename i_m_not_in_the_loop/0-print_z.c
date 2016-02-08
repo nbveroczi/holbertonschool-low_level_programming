35940d7e46a615f960f042d2a28933bac4d79238
nbveroczi <nbveroczi@gmail.com>
2016-02-08 06:49:28 +0000

    commented on the function

--- i_m_not_in_the_loop/0-print_z.c
+++ i_m_not_in_the_loop/0-print_z.c
@@ -1,5 +1,5 @@
 #include "my_functions.h"
-
+#This function prints the lowercase z followed by a new line to standard output with the function print_char
 void print_z(void)
 {
     print_char('z');
