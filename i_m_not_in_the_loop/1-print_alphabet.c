0df42ec91320ed632ca455adae2a98252b392571
nbveroczi <nbveroczi@gmail.com>
2016-02-09 01:04:04 +0000

    upload c project as is - without proper commenting, yet

--- i_m_not_in_the_loop/1-print_alphabet.c
+++ i_m_not_in_the_loop/1-print_alphabet.c
@@ -1,11 +1,11 @@
 #include "my_functions.h"
-/*This is where the comment goes */ 
+/*This file starts with the function prototype which tells the return type of the data that the function will return, and that their is zero (void) number of arguments passed to the function and the data order and increment in which  */ 
 
 void print_alphabet(void)
 {
   char aChar;
 
-  for (aChar = 'a'; aChar <= 'z'; aChar++) 
+  for (aChar = 'a'; aChar < = 'z'; aChar ++) 
   {
     print_char(aChar);
   }
---
+++ i_m_not_in_the_loop/2-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include "my_functions.h"
+/*This is where the comment goes */
+
+void print_tebahpla(void)
+
+{
+  char aChar;
+
+  for (aChar = 'z'; aChar >= 'a'; aChar--)
+  {  
+    print_char(aChar);
+  }
+}
