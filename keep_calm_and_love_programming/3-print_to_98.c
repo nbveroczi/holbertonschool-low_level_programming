ebaeef979d61afc44bac85c54bc4289937315b65
nbveroczi <nbveroczi@gmail.com>
2016-02-20 05:33:42 +0000

    final print 98 update

--- keep_calm_and_love_programming/3-print_to_98.c
+++ keep_calm_and_love_programming/3-print_to_98.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-
+/* This function takes an integer as argument, and prints -
+   one one line - all numbers from it's value to 98, followed
+   by a new line 
+*/
 void print_to_98(int n)
-{
-  for (n = n; n < 98; n++)
+{ 
+  int delta, i;
+  
+  if (n < 98)
     {
-      if (n == 98)
-	{
-	  printf("%d %s\n",n, " ");
-	}
-      else
-	{
-	  printf("%d %s",n,", ");
-        }
+      delta = 1;
     }
+  else if (n > 98)
+    {
+      delta = -1;
+    }
+  else 
+    {
+      delta = 0;
+    }
+  
+  for (i = n; i != 98; i += delta)
+    {
+      printf("%d, ", i);
+    }
+  printf("%d\n", i);
+}    
 
-  for (n = n; n > 97; n--)
-    {
-      if (n == 98)
-	{
-	  printf("%d %s\n",n, " ");
-	}
-      else 
-	{
-          printf("%d %s",n,", ");
-	}
-    }
-}
+
 
