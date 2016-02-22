bb9bd039877e272b16cbcabdbad671408c575bb6
nbveroczi <nbveroczi@gmail.com>
2016-02-22 19:33:22 +0000

    lowercase conversion

---
+++ it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
@@ -0,0 +1,18 @@
+/* This is a function that convers a letter to 
+   lower case, if possible.
+*/
+
+char lowercase(char c)
+{
+  char result;
+
+  if ((c >= 'A') && (c <= 'Z'))
+    {
+      result = c - ('A' - 'a');
+    }
+  else
+    {
+      result = c;
+    }
+  return result;
+}
