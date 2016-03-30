c1beb8150b57c7e6107cd8ca0078427d278902a8
nbveroczi <nbveroczi@gmail.com>
2016-03-30 23:35:31 +0000

    fix filename

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/5-reverse_array.c
@@ -0,0 +1,17 @@
+#include "5-reverse_array.h"
+
+/* this is a function that reverses the content of an array of integers */
+void reverse_array(int *a, int n)
+{
+  int i;
+
+  for (i = 0; src[i] != '\0'; i++)
+    {
+      dest[i] = src[i];
+    }
+  for ( ; i < src[i]; i++)
+    {
+      dest[i] = '\0';
+    }
+  return dest;
+}
