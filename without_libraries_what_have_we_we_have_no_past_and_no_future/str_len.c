96cfa061cffa7f31dee278a47c96468b37398f1c
Naomi Veroczi <nbveroczi@Naomis-MacBook-Pro.local>
2016-04-05 21:28:30 -0700

    add source files for library

---
+++ without_libraries_what_have_we_we_have_no_past_and_no_future/str_len.c
@@ -0,0 +1,14 @@
+#include "str_len.h"
+/* This is a function that returns the length of a string. */
+
+int str_len(char *str)
+{
+  int len;
+  len = 0;
+
+  while (str[len] != '\0')
+    {
+      len++;
+    }
+  return len;
+}
