74ba7f491015f7f0dee898911a56a79788d94c39
Naomi Veroczi <nbveroczi@Naomis-MacBook-Pro.local>
2016-04-29 14:44:01 -0700

    add ) to fix error

--- cisdoublefun_day_2_pointers/4-string_to_integer.c
+++ cisdoublefun_day_2_pointers/4-string_to_integer.c
@@ -12,36 +12,25 @@ int string_to_integer(char *s)
 
   while ((s[i] != '\0') && (s[i] < '0' || s[i] > '9'))
   {
-    printf("%c", s[i]);
     if (s[i] == '-')
     {
       sign *= -1;
     }
-
     i++;
   }
 
   while ((s[i] != '\0') && (s[i] >= '0' && s[i] <= '9'))
   {
-    
-    printf("%c", s[i]);
     number *=10;
     new_digit = s[i] - '0';
     number += new_digit;
-    if ((sign == 1 && number  > INT_MAX) ||
-	(sign == -1 && (-1 * number < INT_MIN))
-      {                                                                                                                     
-	return(0);                                                                                                          
-      }
+    if ((sign == 1 && number  > INT_MAX) || (sign == -1 && (-1 * number < INT_MIN)))
+    {                                    
+      return(0);                                                                   
+    }
 
     i++;
   }
-  /*    if ((sign == 1 && number  > INT_MAX) ||
-	(sign == -1 && (-1 * number < INT_MIN))
-    {                                                                                                          return(0);                                                                                             } 
-      
-  }
-  */
 
   return (sign * number);
 }
