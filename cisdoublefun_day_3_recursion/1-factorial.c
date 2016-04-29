a936ebd4b52dd9f061a980a6acbc32419af5e294
Naomi Veroczi <nbveroczi@Naomis-MacBook-Pro.local>
2016-04-29 15:53:01 -0700

    fix recursive factorial function by adding statement factorial 0 is == 1

--- cisdoublefun_day_3_recursion/1-factorial.c
+++ cisdoublefun_day_3_recursion/1-factorial.c
@@ -3,17 +3,17 @@ This is a RECURSIVE function that returns the factorial of a given number
 Returns -1 (indicating error) if n is < 0, returns factor of Number, returns
 -1 if result can not be stored in an int
 */
-
+#include <limits.h>
 int factorial(int n)
 {
 
-  int result;
-  result = 1;
-
-  if (n < 0)
+  if ((n < 0) || (n > INT_MAX))
   {
     return (-1);
   }
-
+  if (n == 0)
+  {
+    return (1);
+  }
   return (n * factorial(n - 1));
 }
