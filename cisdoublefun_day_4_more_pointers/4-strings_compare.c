8511c5e663c5fdcbce2cf1d8d81efbcb0d37d0c7
Naomi Veroczi <nbveroczi@Naomis-MacBook-Pro.local>
2016-05-01 00:40:55 -0700

    first attempt at strcmp

---
+++ cisdoublefun_day_4_more_pointers/4-strings_compare.c
@@ -0,0 +1,15 @@
+/* Compare Strings
+This is a function that compares two strings.
+*/
+#include <string.h>
+int strings_compare(char *s1, const char *s2)
+{
+  int result;
+  result = strcmp(s1, s2);
+
+  if(result < 0)
+
+  return(result);
+
+
+}
