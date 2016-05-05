09dbed48e3b8d8adb04d6f4c556dd918142c3b25
Naomi Veroczi <nbveroczi@Naomis-MacBook-Pro.local>
2016-05-05 12:16:31 -0700

    first struct exercise

---
+++ macros_and_structures/1-string_to_struct.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include "str_struct.h"
+/* Secure the length of a string */
+
+int str_len(char *str)
+{
+  int len;
+  len = 0;
+  if (str == NULL)
+  {
+    return (0);
+  }
+  while (str[len] != '\0')
+  {
+    len++;
+  }
+
+  return (len);
+}
+
+/* String struct
+This is a function that stores a string and its length in a structure
+Returns the address of this structure
+*/
+
+struct String *string_to_struct(char *str)
+{
+  int length;
+  length = str_len(str);
+
+  struct String *string;
+
+  string = malloc(sizeof(struct String));
+  if (string == NULL)
+    return (NULL);
+  string->str = str;
+  string->length = length;
+
+  return string;
+}
