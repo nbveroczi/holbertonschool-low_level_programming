1897ea4a63ed7e3bb5083d8567b4bb1e3d06f8dc
Naomi Veroczi <nbveroczi@Naomis-MacBook-Pro.local>
2016-05-12 22:37:06 -0700

    string concat with malloc

---
+++ float_like_a_butterfly_sting_like_a_bee/1-string_concat.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "string_concat.h"
+/* String Concat
+This is a function that concatenates two strings.
+Returns a pointer pointing to a newly allocated space in memory which
+contains s1 followed by s2 and null terminated */
+
+char *string_concat(char *s1, char *s2)
+{
+  int index, index2;
+  char *concat = NULL;
+  for (index = 0; s1[index] != '\0'; index++);
+  for (index2 = 0; s2[index2] != '\0'; index++, index2++);
+  concat = malloc(sizeof(*concat) * index++);
+  if (concat == NULL)
+  {
+    return (NULL);
+  }
+  for (index = 0; s1[index] != '\0'; index++)
+  {
+    concat[index] = s1[index];
+  }
+  for (index2 = 0; s2[index2] != '\0'; index2++, index++)
+    {
+    concat[index] = s2[index2];
+    }
+  concat[index] = '\0';
+
+  return (concat);
+}
